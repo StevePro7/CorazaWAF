ea9771561cf438a4f632c6ec597082878666d33f
StevePro7 <steveprozune@hotmail.com>
2021-09-21 15:40:26 +0100

    files

---
+++ Test01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	//coraza "github.com/jptosso/coraza-waf"
+	//_ "github.com/jptosso/coraza-waf/seclang"
+)
+
+// func initCoraza() {
+
+// 	coraza.NewWaf()
+// }
+
+// func parseRules(waf *coraza.Waf) {
+// 	parser, _ := seclang.NewParser(waf)
+// 	parser.FromString(`SecAction "id:1,phase:1,deny:403"`)
+// }
+
+func main() {
+	//initCoraza()
+	fmt.Println("Hello there...!")
+}
